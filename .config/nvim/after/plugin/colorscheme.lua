19886db8efc25fd64b7d0d566d511c205bcf4c8d
Harmy1410 <harmy1410@gmail.com>
2022-12-25 22:23:17 -0500

    transparent signcolumn

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -76,3 +76,4 @@ end
 -- transparent that shit
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
