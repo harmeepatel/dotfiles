87fb1c00a8a6d10fef6ea51c27ab980262dd63f6
Harmy1410 <harmy1410@gmail.com>
2023-03-02 11:20:38 -0500

    clipboard

--- .config/nvim/lua/whatadrag/set.lua
+++ .config/nvim/lua/whatadrag/set.lua
@@ -15,11 +15,6 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
@@ -34,3 +29,9 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.clipboard = 'unnamedplus'
