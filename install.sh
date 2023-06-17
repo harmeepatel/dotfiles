058803bac36746a79e481230ebdab9e35e298a6a
Harmy1410 <harmy1410@gmail.com>
2023-06-16 22:04:39 -0400

    feat: creating install script

--- install.sh
+++ install.sh
@@ -7,13 +7,26 @@ if [ ! -d "/usr/local/Homebrew/" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# brew
+# install brew packages
+brew install alacritty
+brew install btop
+brew install exa
+brew install fzf
 brew install git
+brew install hyperfine
+brew install iina
+brew install lf
+brew install neovim
+brew install stow
+brew install tmux
+brew install trash
+brew install zoxide
 
 # git clone
-if [ ! -d "~/.files" ]; then
-    git clone --depth=1 https://github.com/Harmy1410/mmc.git
+if [ ! -d "/Users/$YOU/dotfiles" ]; then
+    git clone --depth=1 https://github.com/Harmy1410/dotfiles.git
 fi
 
 # stow
+cd dotfiles
 stow .home
