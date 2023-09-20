fad6b840373b697163a16f4bbc9fe5a8ab599523
Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>
2023-09-20 13:55:59 -0400

    run tmux on startup

--- .home/.config/wezterm/tmux.lua
+++ .home/.config/wezterm/tmux.lua
@@ -1,46 +1,54 @@
 local wezterm = require 'wezterm'
-local mux = wezterm.mux
+local io = require 'io'
+-- local mux = wezterm.mux
 local M = {}
 
-function M.run_tmux_on_startup(config)
+local basename = function(str)
+    local name = string.gsub(str, "(.*/)(.*)", "%2")
+    return name
+end
+
+local is_tmux_running = function()
+    local tmux_pid_handle = io.popen("pgrep -o tmux")
+    if tmux_pid_handle == nil then
+        wezterm.log_error "PROBLEM RUNNING `pgrep -o tmux`"
+        return false, true
+    end
+    local tmux_pid = tmux_pid_handle:read("*a")
+    tmux_pid_handle:close()
+    return string.gsub(tmux_pid, "\n", '') ~= '' or false, false
+end
+
+local get_tmux_bin_path = function()
     local tmux_hb_version_handle = io.popen("\\ls /usr/local/Cellar/tmux | awk '{print$0}' | head -1")
-    -- local echo_tmux = string.gsub(io.popen("echo $TMUX"):read("*a"), "%s+", "")
-    local echo_tmux = string.gsub(io.popen("tmux list-sessions &> /dev/null | wc -l"):read("*a"), "%s+", "")
-
-    if tmux_hb_version_handle == '' or tmux_hb_version_handle == nil then
-        wezterm.log_error('tmux_hb_version_handle not found')
-        return config
+    if tmux_hb_version_handle == nil then
+        wezterm.log_error "tmux version handler is nil"
+        return '', true
     end
-
-    if echo_tmux == nil then
-        wezterm.log_error('echo_tmux not found')
-        return config
-    end
-
-    local tmux_hb_read = tmux_hb_version_handle:read("*a")
-
-    local tmux_hb_version = string.gsub(tmux_hb_read, "%s+", "") -- remove whitespaces '%s+'
-    local tmux_bin_path = '/usr/local/Cellar/tmux/' .. tmux_hb_version .. '/bin/tmux'
-
-
-    wezterm.log_info("TMUX: " .. echo_tmux)
-
-    -- if echo_tmux == '' then
-    --     wezterm.log_info("inside: if tmux_env == nil")
-    --     wezterm.log_info("TMUX: " .. echo_tmux)
-    --     -- config.default_prog = { tmux_bin_path, 'new', '-s', os.getenv("USER") }
-    -- else
-    --     config.default_prog = { tmux_bin_path, 'a' }
-    -- end
+    local tmux_bin_path = "/usr/local/Cellar/tmux/" ..
+        string.gsub(tmux_hb_version_handle:read("*a"), "\n", '') ..
+        "/bin/tmux"
 
     tmux_hb_version_handle:close()
+    return tmux_bin_path
+end
 
-    wezterm.on('gui-startup', function(cmd)
-        local _tab, _pane, window = mux.spawn_window({
-            args = { tmux_bin_path, 'a' }
-        } or {})
-        -- window:gui_window()
-    end)
+function M.run_tmux_on_startup(config)
+    local tmux_bin_path, bin_path_err = get_tmux_bin_path()
+    if bin_path_err then
+        return config
+    end
+
+    local tmux_running, tmux_pid_err = is_tmux_running()
+    if tmux_pid_err then
+        return config
+    end
+
+    if tmux_running then
+        config.default_prog = { tmux_bin_path, 'a' }
+    else
+        config.default_prog = { tmux_bin_path, 'new', '-s', basename(os.getenv("HOME")) }
+    end
 
     return config
 end
