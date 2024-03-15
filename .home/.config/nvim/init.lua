ddda8188e1856a74b79dd524603b9633092dbc29
Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>
2024-03-15 13:45:59 -0400

    converting hl yank vimscript -> lua

--- .home/.config/nvim/init.lua
+++ .home/.config/nvim/init.lua
@@ -3,12 +3,18 @@ require("whatadrag.remap")
 require("whatadrag.lazy")
 -- require("whatadrag.custom")
 
-vim.cmd [[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]]
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 -- no commenting new line
 vim.cmd([[autocmd BufEnter * set formatoptions-=o]])
+
+
+vim.filetype.add({ extension = { templ = "templ" } })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = vim.lsp.buf.format })
