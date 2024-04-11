1f7e3f9e09b44f8d9f9cbd926114a5302be83b75
Harmee Patel <daddy_davinci@Harmees-MBP.hitronhub.home>
2024-04-11 10:23:14 -0400

    minor

--- .home/.config/nvim/lua/whatadrag/set.lua
+++ .home/.config/nvim/lua/whatadrag/set.lua
@@ -12,6 +12,18 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+vim.opt.undofile = true
+vim.opt.clipboard = 'unnamedplus'
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -20,18 +32,8 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
-vim.opt.undofile = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.pumheight = 16
