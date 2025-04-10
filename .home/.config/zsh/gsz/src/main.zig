04c0259be43267aa5762bc4df0f0f5e078f6b5ad
Harmee Patel <harmeepatel@gmail.com>
2025-04-09 21:43:24 -0400

    [major] feat: fixed git status and adding color to status

--- .home/.config/zsh/gsz/src/main.zig
+++ .home/.config/zsh/gsz/src/main.zig
@@ -1,41 +1,64 @@
 const std = @import("std");
 const root = @import("root.zig");
-const dbg_print = std.debug.print;
+const print = std.debug.print;
+const stdout = std.io.getStdOut().writer();
+const hash = std.hash.Wyhash.hash;
 
 const BUF_SIZE = 128 * 128;
-const LINE_SIZE = 128;
+
+fn rgbFromHex(hex_code: []const u8) [3]u32 {
+    const user_hex = if (hex_code[0] == '#') hex_code[1..] else hex_code;
+    const hex_final = if (user_hex.len == 3) &[_]u8{ user_hex[0], user_hex[0], user_hex[1], user_hex[1], user_hex[2], user_hex[2] } else user_hex;
+    const hex_int = std.fmt.parseInt(u32, hex_final, 16) catch 0;
+    return [_]u32{
+        (hex_int >> 16) & 0xFF, // red
+        (hex_int >> 8) & 0xFF, // green
+        hex_int & 0xFF, // blue
+    };
+}
+
+fn wrapAnsi16m(alloc: std.mem.Allocator, rgb: [3]u32) []const u8 {
+    return std.fmt.allocPrint(alloc, "\u{001B}[38;2;{};{};{};m", .{ rgb[0], rgb[1], rgb[2] }) catch "";
+}
 
 pub fn main() !void {
+    // command
     const git_status_cmd = [_][]const u8{ "git", "status", "-s" };
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // allocator
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    var process = std.ChildProcess.init(&git_status_cmd, allocator);
+    // process
+    var process = std.process.Child.init(&git_status_cmd, allocator);
     process.stdout_behavior = .Pipe;
+    process.stderr_behavior = .Pipe;
     try process.spawn();
+    defer _ = process.kill() catch {};
 
-    var buffer: [BUF_SIZE]u8 = undefined;
-    const n_bytes = try process.stdout.?.readAll(&buffer);
+    // exit early if `git status` exits with an error
+    const err = try process.stderr.?.readToEndAlloc(allocator, BUF_SIZE);
+    if (err.len > 0) {
+        try stdout.print("", .{});
+        std.process.exit(0);
+    }
 
-    var gs = std.AutoArrayHashMap(u8, u8).init(allocator);
+    // stdout
+    const out = try process.stdout.?.readToEndAlloc(allocator, BUF_SIZE);
+    defer allocator.free(out);
+
+    // status map
+    var gs = std.StringHashMap(u8).init(allocator);
     defer gs.deinit();
 
-    var i: usize = 0;
-    while (i < n_bytes) : (i += 1) {
-        var line = std.ArrayList(u8).init(allocator);
-        defer line.deinit();
-
-        var three_chars_flag: usize = 0;
-        while (buffer[i] != '\n') : (i += 1) {
-            if (three_chars_flag == 3) {
-                continue;
-            }
-            _ = try line.append(buffer[i]);
-            three_chars_flag += 1;
+    var line_split = std.mem.splitScalar(u8, out, '\n');
+    while (line_split.next()) |line| {
+        if (line.len == 0) {
+            continue;
         }
-        const status_symbol = std.mem.trim(u8, line.items, " ");
-        const status = try gs.getOrPut(status_symbol[0]);
+        const status_split = std.mem.trim(u8, line[0..3], " ");
+        const status = try gs.getOrPut(status_split);
 
         if (status.found_existing) {
             status.value_ptr.* += 1;
@@ -44,18 +67,22 @@ pub fn main() !void {
         }
     }
 
-    // var output: [16]u8 = undefined;
-    i = 0;
+    var i: u16 = 0;
     var it = gs.iterator();
-    const stdout_writer = std.io.getStdOut().writer();
-    while (it.next()) |val| : (i += 1) {
-        _ = try std.fmt.format(stdout_writer, "{d}{c}", .{ val.value_ptr.*, val.key_ptr.* });
-        if (i < it.len - 1) {
-            _ = try std.fmt.format(stdout_writer, " ", .{});
+    const color_delete = "#ff6637";
+    const color_modified = "#ffaa24";
+    const color_untracked = "#7788ff";
+    while (it.next()) |entry| : (i += 1) {
+        const col = switch (hash(0, entry.key_ptr.*)) {
+            hash(0, "D") => wrapAnsi16m(allocator, rgbFromHex(color_delete)),
+            hash(0, "M") => wrapAnsi16m(allocator, rgbFromHex(color_modified)),
+            hash(0, "??") => wrapAnsi16m(allocator, rgbFromHex(color_untracked)),
+            else => "#fefefe",
+        };
+        defer allocator.free(col);
+        try stdout.print("{s}\u{001B}[1m{d}{s}\u{001B}[22m", .{ col, entry.value_ptr.*, entry.key_ptr.* });
+        if (i < gs.unmanaged.size - 1) {
+            try stdout.print(" ", .{});
         }
     }
-}
-
-test "simple test" {
-    dbg_print("test\n", .{});
 }
