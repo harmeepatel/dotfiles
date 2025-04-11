6a7bfd9cdc1823c9dfca21cd6184e7276db28747
Harmee Patel <harmeepatel@gmail.com>
2025-04-10 21:37:42 -0400

    [major] feat: moving to my prompt fixing zig bin

--- .home/.config/zsh/gsz/src/main.zig
+++ .home/.config/zsh/gsz/src/main.zig
@@ -1,24 +1,13 @@
 const std = @import("std");
 const root = @import("root.zig");
-const print = std.debug.print;
+const debug = std.log.debug;
 const stdout = std.io.getStdOut().writer();
 const hash = std.hash.Wyhash.hash;
 
 const BUF_SIZE = 128 * 128;
 
-fn rgbFromHex(hex_code: []const u8) [3]u32 {
-    const user_hex = if (hex_code[0] == '#') hex_code[1..] else hex_code;
-    const hex_final = if (user_hex.len == 3) &[_]u8{ user_hex[0], user_hex[0], user_hex[1], user_hex[1], user_hex[2], user_hex[2] } else user_hex;
-    const hex_int = std.fmt.parseInt(u32, hex_final, 16) catch 0;
-    return [_]u32{
-        (hex_int >> 16) & 0xFF, // red
-        (hex_int >> 8) & 0xFF, // green
-        hex_int & 0xFF, // blue
-    };
-}
-
-fn wrapAnsi16m(alloc: std.mem.Allocator, rgb: [3]u32) []const u8 {
-    return std.fmt.allocPrint(alloc, "\u{001B}[38;2;{};{};{};m", .{ rgb[0], rgb[1], rgb[2] }) catch "";
+fn addStyle(alloc: std.mem.Allocator, col: u8) []const u8 {
+    return std.fmt.allocPrint(alloc, "%F{{{}}}", .{col}) catch "";
 }
 
 pub fn main() !void {
@@ -26,9 +15,10 @@ pub fn main() !void {
     const git_status_cmd = [_][]const u8{ "git", "status", "-s" };
 
     // allocator
-    var gpa: std.heap.DebugAllocator(.{}) = .init;
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    var buf: [1 << 14]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buf);
+    const allocator = fba.allocator();
+    defer allocator.free(&buf);
 
     // process
     var process = std.process.Child.init(&git_status_cmd, allocator);
@@ -47,6 +37,11 @@ pub fn main() !void {
     // stdout
     const out = try process.stdout.?.readToEndAlloc(allocator, BUF_SIZE);
     defer allocator.free(out);
+
+    if (out.len == 0) {
+        try stdout.print("", .{});
+        std.process.exit(0);
+    }
 
     // status map
     var gs = std.StringHashMap(u8).init(allocator);
@@ -67,20 +62,21 @@ pub fn main() !void {
         }
     }
 
+    // constructing output
     var i: u16 = 0;
     var it = gs.iterator();
-    // const color_delete = "#ff6637";
-    // const color_modified = "#ffaa24";
-    // const color_untracked = "#7788ff";
+    const color_delete = 1;
+    const color_modified = 2;
+    const color_untracked = 4;
     while (it.next()) |entry| : (i += 1) {
-        // const col = switch (hash(0, entry.key_ptr.*)) {
-        //     hash(0, "D") => wrapAnsi16m(allocator, rgbFromHex(color_delete)),
-        //     hash(0, "M") => wrapAnsi16m(allocator, rgbFromHex(color_modified)),
-        //     hash(0, "??") => wrapAnsi16m(allocator, rgbFromHex(color_untracked)),
-        //     else => "#fefefe",
-        // };
-        // defer allocator.free(col);
-        try stdout.print("{s}{d}{s}", .{ "", entry.value_ptr.*, entry.key_ptr.* });
+        const col = switch (hash(0, entry.key_ptr.*)) {
+            hash(0, "D") => addStyle(allocator, color_delete),
+            hash(0, "M") => addStyle(allocator, color_modified),
+            hash(0, "??") => addStyle(allocator, color_untracked),
+            else => addStyle(allocator, 0),
+        };
+        defer allocator.free(col);
+        try stdout.print("{s}{d}{s}", .{ col, entry.value_ptr.*, entry.key_ptr.* });
         if (i < gs.unmanaged.size - 1) {
             try stdout.print(" ", .{});
         }
