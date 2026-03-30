817d9a8f56a82564b040f1802d19549427365b45
Harmee Patel <harmeepatel@gmail.com>
2026-03-30 11:14:11 -0400

    feat: adding keybinds to focus windows in split mode (left-right)

---
+++ .home/.hammerspoon/Spoons/focus_window_in_split.lua
@@ -0,0 +1,30 @@
+local function getVisibleWindows()
+    local winFilter = hs.window.filter.new():setCurrentSpace(true)
+    return winFilter:getWindows()
+end
+
+local function focusLeftWindow()
+    local wins = getVisibleWindows()
+    if #wins == 0 then return end
+
+    table.sort(wins, function(a, b)
+        return a:frame().x < b:frame().x
+    end)
+
+    wins[1]:focus()
+end
+
+local function focusRightWindow()
+    local wins = getVisibleWindows()
+    if #wins == 0 then return end
+
+    table.sort(wins, function(a, b)
+        return a:frame().x < b:frame().x
+    end)
+
+    wins[#wins]:focus()
+end
+
+-- Keybindings
+hs.hotkey.bind({ "ctrl" }, "h", focusLeftWindow)
+hs.hotkey.bind({ "ctrl" }, "l", focusRightWindow)
--- .home/.hammerspoon/init.lua
+++ .home/.hammerspoon/init.lua
@@ -1,3 +1,5 @@
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
     hs.alert.show("Hello World!")
 end)
+
+require('Spoons.focus_window_in_split')
